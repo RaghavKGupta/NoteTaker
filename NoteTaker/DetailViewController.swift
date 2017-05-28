25aaa2565f41116a67c4e2c7e1fea791fb1594e7
Raghav Gupta <rgupta11@terpmail.umd.edu>
2017-05-28 03:51:08 -0400

    Add files via upload

---
+++ NoteTaker/DetailViewController.swift
@@ -0,0 +1,52 @@
+//
+//  DetailViewController.swift
+//  NoteTaker
+//
+//  Created by Raghav Gupta on 5/27/17.
+//  Copyright © 2017 Raghav Gupta. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+    @IBOutlet weak var textView: UITextView!
+    var text:String = ""
+    var masterView:ViewController!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        textView.text = text
+        textView.becomeFirstResponder()
+    }
+    
+    func setText(t:String){
+        text = t
+        if isViewLoaded{
+            textView.text = t
+        }
+    }
+
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        masterView.newRoxText = textView.text
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
